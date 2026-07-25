32aa6f8402da6d5edc834c771bb4651e42fd4848
Giddytech <giddytechent@gmail.com>
2026-07-25 18:26:37 +0100

    first commit

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,36 @@
+import express, {Request, Response} from "express"
+import cors from "cors"
+
+import { clerkMiddleware, getAuth } from '@clerk/express'
+
+const app = express()
+app.use(cors({
+    origin:["http://localhost:3002","http://localhost:3003"], 
+    credentials:true
+}))
+
+app.use(clerkMiddleware())
+
+app.get("/health",(req:Request,res:Response)=>{
+    res.json({
+        status: 'ok',
+        uptime: process.uptime(),
+        timestamp: Date.now()
+    })
+})
+
+app.get("test",(req,res)=>{
+    const { isAuthenticated, userId } = getAuth(req)
+
+    if (!userId){
+        return res.status(401).json({message: "You are not authenticated"})
+    }
+    
+    res.json({
+        message: "Product service autheticated"
+    })
+})
+
+app.listen(8000, ()=>{
+    console.log("Product service is running on port 8000")
+})
